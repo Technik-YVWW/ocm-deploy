ea042c578452ec54fb7e3b8a9923abcc6f48bc6a
it@westerwald.yoga-vidya.de <it@westerwald.yoga-vidya.de>
2023-09-14 01:40:04 +0200

    Autocomment: Adding files from PC: WW-Akama, User: damodara On 2023-09-14, at 01:40:04

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 Quellcode automatisch über inotify und Ansible auf Anderen Geräten Deployen
 
 Vorrausetzungen, installiert:
+  * yq [NICHT jq! das ist für json, yq ist yaml]
   * intoifywait
   * ansible
--- install.sh
+++ install.sh
@@ -4,3 +4,23 @@
 # Verwende "readlink" für absolute Pfade und "dirname" für relative Pfade
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 cd $SCRIPT_DIR
+
+echo Checking Foldrs...
+folders="/etc/deploycode/configs-available /etc/deploycode/configs-enabled /etc/deploycode/playbooks /etc/deploycode/playbook-vars /usr/lib/libDeploy"
+for folder in $folders; do
+    echo Folder: $folder
+    [ ! -d "$folder" ] && mkdir -p $folder
+done
+
+echo "Installig Mainscript..."
+cp -fv deploycode /usr/bin
+
+echo Installing Libaries..
+cp -fv usr/lib/libDeploy/sh_functions /usr/lib/libDeploy/
+
+for file in $(ls ./etc/deploycode/playbooks); do
+    [ -f "./etc/deploycode/playbooks/$file" ] && {
+        echo file: $file
+        cp -rv ./etc/deploycode/playbooks/$file /etc/deploycode/playbooks
+    }
+done
